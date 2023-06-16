e7e3e6b3710638bcffeddc2aa36c9fbf4a97ff34
mKevinSF <kevinsafaat007@gmail.com>
2023-06-16 11:03:24 +0700

    dwadawwa

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 
       <div class="box">
          <h3>Kelompok 2</h3>
-         <a href="#"> <i class="fas fa-user"></i> 2540117621  - Jonathan Lim </a>
+         <a href="#"> <i class="fas fa-user"></i> 2540117621  - Jonathan</a>
          <a href="#"> <i class="fas fa-user"></i> 2540133663  - Muhammad Kevin Safaat </a>
          <a href="#"> <i class="fas fa-user"></i> 2502032094 - Salman Al Farisy Azhar </a>
          <a href="#"> <i class="fas fa-user"></i> 2540130825  - Stanley Sen Riyanto</a>
