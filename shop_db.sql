75635ae3111aa66990dd455f26d65f8be2678887
hawlim257 <jonathan.lim002@binus.ac.id>
2023-06-16 21:32:14 +0700

    sql

--- shop_db.sql
+++ shop_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 16, 2023 at 11:11 AM
+-- Generation Time: Jun 16, 2023 at 04:32 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -36,6 +36,14 @@ CREATE TABLE `cart` (
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `name`, `price`, `quantity`, `image`) VALUES
+(84, 17, 'Membersihkan AC', 45000, 0, 'bersihinAC.jpg'),
+(85, 17, 'Mencuci Piring', 45000, 0, 'cuciPiring.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -56,14 +64,6 @@ CREATE TABLE `orders` (
   `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
-(20, 15, '123', '12345666', 'lim@mail.com', 'cash on delivery', 'Jl.1234, 1234, 1234, 1234, 1234 - 12345', ', wew () ', 12345, '16-Jun-2023', 'pending'),
-(21, 15, '123', '124', '123@mail.com', 'cash on delivery', 'Jl.123, 123, 123, 123, 123 - 123', ', wew () ', 12345, '16-Jun-2023', 'pending');
-
 -- --------------------------------------------------------
 
 --
@@ -82,7 +82,13 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `image`) VALUES
-(25, 'wew', 12345, '2c7ab31d-501b-4749-8eac-09d084b827e8-1.jpg');
+(30, 'Mencuci Baju', 50000, 'cuciLipatStrika.jpg'),
+(31, 'Membersihkan AC', 45000, 'bersihinAC.jpg'),
+(32, 'Mencuci Mobil', 35000, 'cuciMobil.jpg'),
+(33, 'Mencuci Piring', 45000, 'cuciPiring.jpg'),
+(34, 'Mengepel Lantai', 55000, 'sapu&PelRumah.jpg'),
+(35, 'Membersihkan Halaman', 45000, 'sapuHalaman.jpg'),
+(36, 'All In One', 150000, 'allInOne.jpg');
 
 -- --------------------------------------------------------
 
@@ -103,14 +109,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`) VALUES
-(8, 'user', 'user@gmail.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'user'),
-(9, 'admin', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
-(10, 'kevin', 'kevin@gmail.com', 'd2e7a2105d0fb461fe6f2858cc33942f', 'user'),
-(11, 'matthew', 'matthew@mail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'user'),
-(12, 'Jonathan Lim', 'jonathan.lim002@binus.ac.id', '25d55ad283aa400af464c76d713c07ad', 'user'),
-(13, '123', 'h4uh4w@gmail.com', 'dae39c9c090100815489f4f44a956399', 'user'),
-(14, 'admin', 'admin@mail.com', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
-(15, 'lim', 'lim@mail.com', '499e0d7e3f2f15a72fb4d114388bcb0a', 'user');
+(16, 'admin', 'admin@mail.com', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
+(17, 'user', 'user@mail.com', 'ee11cbb19052e40b07aac0ca060c23ee', 'user');
 
 --
 -- Indexes for dumped tables
@@ -148,7 +148,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -160,13 +160,13 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
