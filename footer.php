4f4db18cd91526aad920bef5ab61bcf2dc7a7048
Muhammad Kevin Safaat <89779132+mKevinSF@users.noreply.github.com>
2023-06-21 20:25:20 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
       <div class="box">
          <h3>Kelompok 1</h3>
          <a href="https://www.youtube.com/watch?v=QdezFxHfatw"> <i class="fas fa-user"></i> 2540117621   - Jonathan Lim</a>
-         <a href="https://www.youtube.com/watch?v=rV5ynCW-kVw&ab_channel=ZxiosExe"> <i class="fas fa-user"></i> 2540133663   - Muhammad Kevin Safaat </a>
+         <a href="#"> <i class="fas fa-user"></i> 2540133663   - Muhammad Kevin Safaat </a>
          <a href="#"> <i class="fas fa-user"></i> 2540117565   - Calvin Nelson </a>
          <a href="#"> <i class="fas fa-user"></i> 2540117514   - Matthew Alexander Christanto</a>
          <a href="https://www.youtube.com/watch?v=yPYZpwSpKmA"> <i class="fas fa-user"></i> 2540132225   - Vincent Antonius Pandora </a>
